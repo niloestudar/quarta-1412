58266a1300f710df87e18fc6542cbd28269a2cbd
Técnico em Informática 2022.2 <36127512022.2@senacrjedu.local>
2022-12-14 16:23:09 -0300

    first commiDDDt

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$num1 = 100;
+$num2 = 200;
+
+$soma = $num1 + $num2;
+
+if($soma >= 200){
+    echo"Maior que 200";
+}
+else{
+    echo "Menor que 200";
+}
+
+echo "<br/> o resultado da soma é {$soma}";
+
+?>
+
+<html>
+<header>
+    <?php echo "Hello World"; ?>
+</header>
+</html>
